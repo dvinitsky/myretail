060905ca4be424b5213c0400c1c478c774f59709
dvinitsky <daniel.vinitsky@gmail.com>
2018-07-18 20:04:33 -0500

    Build api call and basic product page displaying

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const bodyParser = require('body-parser');
+const fetch = require("node-fetch");
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const url = 'mongodb://heroku_75zdms2r:3rfek657p2ha1h7dskti6ovug3@ds143511.mlab.com:43511/heroku_75zdms2r';
 const dbName = 'heroku_75zdms2r';
 
-// Start MongoDB connection
+// API Endpoint provided for test case //
+const testId = 13860428;
+
+
+//////////////////////////////////////////
+
+// START MONGODB CONNECTION //
 MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
   assert.equal(null, err);
   console.log("Connected successfully to some Mongo server!");
@@ -22,8 +33,35 @@ MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
 
   // BEGIN ROUTES //
   app.get('/', (req, res) => {
-    res.render('pages/main');
+    res.render('pages/main', { message: 'No products to show.' });
   });
+
+  app.get('/products/:id', (req, res) => {
+    let id = req.params.id;
+
+
+    // Calls Redsky API to get product info //
+    async function getData(id) {
+      try {
+        let response = await fetch(`https://redsky.target.com/v2/pdp/tcin/13860428`);
+        if (response.ok) {
+          let jsonResponse = await response.json();
+          console.log(jsonResponse);
+          return jsonResponse;
+        }
+        throw new Error('Request failed.');
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getData(id);
+
+
+
+    res.render('pages/main', { message: req.params.id });
+  });
+
 
 
 
