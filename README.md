2009e279c3bb6907af2e32c09c8ec537a9bc7b5f
dvinitsky <daniel.vinitsky@gmail.com>
2018-07-20 16:11:07 -0500

    mongodb testing

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ To access, send an HTTP request to "https://vinitsky-myretail.herokuapp.com/prod
 The API will return a JSON object in the following format:
 {"id":{id},"name":{product name},"current_price":{"value": {price},"currency_code":{currency code}}}
 
-Product ID and name are pulled from the Target.com product page, i.e. https://www.target.com/p/the-big-lebowski-blu-ray/-/A-13860428. The price info is pulled from my MongoDB database hosted in mLab, hosted on Heroku.
+Product ID and name are pulled from the Target.com product page, e.g. https://www.target.com/p/the-big-lebowski-blu-ray/-/A-13860428. The price info is pulled from my MongoDB database hosted in mLab, hosted on Heroku.
 
 The following IDs are available in the MongoDB databse for this case study:  
 13860428  
--- index.js
+++ index.js
@@ -3,58 +3,90 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const MongoClient = require('mongodb').MongoClient;
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const fetch = require("node-fetch");
-
-//app.use(bodyParser.urlencoded({ extended: false }));
 
 const url = 'mongodb://heroku_75zdms2r:3rfek657p2ha1h7dskti6ovug3@ds143511.mlab.com:43511/heroku_75zdms2r';
 const dbName = 'heroku_75zdms2r';
 
-const testExclude = 'taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics';
+const excludeForTest = 'taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics';
 
+let db;
 
-// START MONGODB CONNECTION //
 MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
   if (err) throw err;
   console.log("Connected successfully to Mongo server!");
 
-  const db = client.db(dbName);
-  const collection = db.collection('products');
-
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'ejs');
-
-  // BEGIN ROUTES //
-  app.get('/products/:id', (req, res) => {
-    // Call Redsky API to get product info //
-    (async () => {
-      try {
-        let response = await fetch(`https://redsky.target.com/v2/pdp/tcin/${req.params.id}?excludes=${testExclude}`);
-        if (response.ok) {
-          let jsonResponse = await response.json();
-
-          let productName = jsonResponse.product.item['product_description'].title;
-
-          collection.findOne({ 'id': 11346672 }, (err, result) => {
-            if (err) throw err;
-            let priceInfo = result['current_price'];
-
-            res.send({ 'id': req.params.id, 'name': productName, 'current_price': priceInfo });
-          });
-
-          return;
-        } throw new Error('Request failed.');
-      } catch (error) {
-        console.log(error);
-      }
-    })();
-  });
-  // End of HTTP call
+  db = client.db(dbName);
 
   app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 });
-// End of MongoDB connection
+// *** Do not explicitly close database connection- this will mess up the MongoDB call within the GET request. Connection should close automatically. 
 
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.get('/products/:id', (req, res) => {
+  // Call Redsky API to get product info
+  (async () => {
+    try {
+      let response = await fetch(`https://redsky.target.com/v2/pdp/tcin/${req.params.id}?excludes=${excludeForTest}`);
+
+      if (response.ok) {
+        let jsonResponse = await response.json();
+
+        let productName = jsonResponse.product.item['product_description'].title;
+
+        let collection = db.collection('products');
+
+        collection.findOne({ 'id': req.params.id }, (err, result) => {
+          if (err) throw err;
+          res.send(result);
+          //////////STUCK HERE NOT GETTINGA RESULT 
+        });
+
+        /*
+        { 'id': req.params.id }, 
+        (err, result) => {
+          if (err) throw err;
+          console.log(result);
+          let priceInfo = result['current_price'];
+
+          res.send({ 'id': req.params.id, 'name': productName, 'current_price': priceInfo });
+          */
+
+        return;
+      } throw new Error('Request failed.');
+    } catch (error) {
+      console.log(error);
+    }
+  })();
+});
+
+
+app.post('/products', (req, res) => {
+  let id = req.body.id;
+  let price = req.body.price;
+
+  const collection = db.collection('products');
+  /*
+    collection.updateOne({ 'id': id }, { $set: { name: price } }, (err, result) => {
+      if (err) throw err;
+  
+      console.log(result);
+      return;
+    });
+    // res.render('updated.ejs', { name: name, price: price });
+  */
+});
+
+app.post('/products/:id', (req, res) => {
+});
